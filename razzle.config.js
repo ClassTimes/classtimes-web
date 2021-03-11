7cb5ae999ef3ac56ee3f2ca5984227422dbda370
Gaston Morixe <gaston@gastonmorixe.com>
2021-03-11 17:09:51 -0300

    Heroku fix

--- razzle.config.js
+++ razzle.config.js
@@ -1,12 +1,16 @@
+"use strict"
+
 // https://razzlejs.org/docs/customization
+// https://github.com/jaredpalmer/razzle/blob/canary/examples/with-heroku/razzle.config.js
 
 module.exports = {
-  // options: {
-  //   cssPrefix: 'static/css',
-  //   jsPrefix: 'static/js',
-  //   mediaPrefix: 'static/media',
-  //   browserslist: undefined, // or what your apps package.json says
-  // },
+  options: {
+    //   cssPrefix: 'static/css',
+    //   jsPrefix: 'static/js',
+    //   mediaPrefix: 'static/media',
+    //   browserslist: undefined, // or what your apps package.json says
+    forceRuntimeEnvVars: ["HOST", "PORT"],
+  },
   // modify: require("razzle-heroku"),
   experimental: {
     newBabel: true,
