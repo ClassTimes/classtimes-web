e64bbff9fc51637b3806fa11460353808340fe06
Gaston Morixe <gaston@gastonmorixe.com>
2021-03-22 10:19:30 -0300

    Fix favicons

---
+++ src/assets/favicons.ts
@@ -0,0 +1,12 @@
+export const FAVICONS = [
+  '<link rel="shortcut icon" href="/favicon.ico?v=2">',
+  '<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=2">',
+  '<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=2">',
+  '<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=2">',
+  '<link rel="manifest" href="/site.webmanifest">',
+  '<link rel="mask-icon" href="/safari-pinned-tab.svg?v=2" color="#2825A6">',
+  '<meta name="apple-mobile-web-app-title" content="ClassTimes">',
+  '<meta name="application-name" content="ClassTimes">',
+  '<meta name="msapplication-TileColor" content="#ffffff">',
+  '<meta name="theme-color" content="#ffffff">',
+]
